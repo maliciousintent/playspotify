b09fbb0952db5b551c3888a484ee65f00c0f53d1
Simone Lusenti <lusenti.s@gmail.com>
2013-10-19 19:36:52 +0200

    lint

--- consumer.js
+++ consumer.js
@@ -1,48 +1,37 @@
-/*jshint node:true, laxcomma:true */
+/*jshint node:true, laxcomma:true, unused:true, undef:true, indent:2 */
 
 'use strict';
 
-var lame = require('lame');
-var Speaker = require('speaker');
-var Spotify = require('spotify-web');
- 
+var lame = require('lame')
+  , Speaker = require('speaker')
+  , Spotify = require('spotify-web')
+  , swallow = require('swallow')
+  , pubnub = require('pubnub')
+  ;
 
-
-module.exports = function (SPOTIFY_USERNAME, SPOTIFY_PASSWORD, PUBNUB_SUBSCRIBE_KEY, PUBNUB_PUBLISH_KEY, PUBNUB_CHANNEL) {
-	Spotify.login(SPOTIFY_USERNAME, SPOTIFY_PASSWORD, function (err, spotify) {
-	  if (err) {
-	    throw err;
-	  }
-
-	  var pubnub = require('pubnub').init({
-	    subscribe_key: PUBNUB_SUBSCRIBE_KEY
-	  , publish_key: PUBNUB_PUBLISH_KEY
-	  });
-	  
-	  console.log('waiting for messages');
-	  
-	  pubnub.subscribe({
-	    channel  : PUBNUB_CHANNEL,
-	    connect: function () { console.log('PubNub connected.');   },
-	    callback : function(message) {
-	      console.log('got message', message);
-	      spotify.get(message.uri, function (err, track) {
-	        if (err) throw err;
-	        console.log('Playing: %s - %s', track.artist[0].name, track.name);
-
-	        // play() returns a readable stream of MP3 audio data
-	        track.play()
-	          .pipe(new lame.Decoder())
-	          .pipe(new Speaker())
-	          .on('finish', function () {
-	            spotify.disconnect();
-	          });
-
-	      });
-	    }
-	  });
-
-	  
-	  
-	});	
-}
+module.exports = function _module(SPOTIFY_USERNAME, SPOTIFY_PASSWORD, PUBNUB_SUBSCRIBE_KEY, PUBNUB_PUBLISH_KEY, PUBNUB_CHANNEL) {
+  
+  Spotify.login(SPOTIFY_USERNAME, SPOTIFY_PASSWORD, swallow('while logging into Spotify', function (spotify) {
+    var pn = pubnub.init({
+      subscribe_key: PUBNUB_SUBSCRIBE_KEY
+    , publish_key: PUBNUB_PUBLISH_KEY
+    });
+        
+    pn.subscribe({
+      channel: PUBNUB_CHANNEL,
+      connect: function () { /* @TODO: handle errors */ },
+      callback: function (message) {
+        spotify.get(message.uri, swallow('while retrieving Spotify track', function (track) {
+          console.log('Playing: %s - %s', track.artist[0].name, track.name);
+          
+          track.play()
+            .pipe(new lame.Decoder())
+            .pipe(new Speaker());
+            // .on('finish', function () {
+            //   spotify.disconnect();
+            // });
+        }));
+      }
+    });
+  }));
+};
