f00c7f625524f8f1409da53dec0cf52249517e8f
Simone Lusenti <lusenti.s@gmail.com>
2013-10-20 16:37:55 +0200

    Working spotify search

--- consumer.js
+++ consumer.js
@@ -7,9 +7,13 @@ var lame = require('lame')
   , Spotify = require('spotify-web')
   , swallow = require('node-swallow')
   , pubnub = require('pubnub')
+  , xml2js = require('xml2js')
+  , util = require('./util')
   ;
 
 module.exports = function _module(SPOTIFY_USERNAME, SPOTIFY_PASSWORD, PUBNUB_SUBSCRIBE_KEY, PUBNUB_PUBLISH_KEY, PUBNUB_CHANNEL) {
+  
+  var spkr = new Speaker();
   
   Spotify.login(SPOTIFY_USERNAME, SPOTIFY_PASSWORD, swallow('while logging into Spotify', function (spotify) {
     var pn = pubnub.init({
@@ -21,17 +25,50 @@ module.exports = function _module(SPOTIFY_USERNAME, SPOTIFY_PASSWORD, PUBNUB_SUB
       channel: PUBNUB_CHANNEL,
       connect: function () { /* @TODO: handle errors */ },
       callback: function (message) {
-        spotify.get(message.uri, swallow('while retrieving Spotify track', function (track) {
-          console.log('Playing: %s - %s', track.artist[0].name, track.name);
+        
+        if (message.search !== undefined) {
+          console.log('Searching track', message.search);
           
-          track.play()
-            .pipe(new lame.Decoder())
-            .pipe(new Speaker());
-            // .on('finish', function () {
-            //   spotify.disconnect();
-            // });
-        }));
+          spotify.search(message.search, swallow('while searching for a track', function (xml) {
+            var parser = new xml2js.Parser();
+            parser.on('end', function (data) {
+              var t_count = parseInt(data.result['total-tracks'][0], 10)
+              
+              if (t_count < 1) {
+                console.log('No track found for search "%s"', search);
+              } else {
+                var track = data.result.tracks[0].track[0] // id,title,artist,album,year
+                  , uri = 'spotify:track:' + util.base62.fromHex(track.id[0], 22)
+                  ;
+                  
+                console.log('Found track', track.title[0], 'by', track.artist[0], '(' + track.album[0] + ', ' + track.year[0] + ')');
+                                
+                spotify.get(uri, swallow('while getting track details', function (track) {
+                  console.log('Playing: %s - %s', track.artist[0].name, track.name);
+                  _playTrack(track);
+                }));
+              }
+            });
+            parser.parseString(xml);
+          }));
+          
+        } else {
+          spotify.get(message.uri, swallow('while retrieving Spotify track', function (track) {
+            console.log('Playing: %s - %s', track.artist[0].name, track.name);
+            _playTrack(track);
+          }));
+        }
       }
     });
   }));
+
+  function _playTrack(track) {
+    track.play()
+      .pipe(new lame.Decoder())
+      .pipe(spkr);
+      // .on('finish', function () {
+      //   spotify.disconnect();
+      // });
+  }
+
 };
